d4419832513027615086708d65a0470ed0fde9f6
EnginKosure <engin.kosure@cronos.be>
2021-09-04 12:10:06 +0200

    add find titles

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-link = "https://www.ebay.com/"
+link = "https://www.ebay.com/e/fashion/bo7-omega-021720"
 
 my_content = requests.get(link)
 
 soup = BeautifulSoup(my_content, 'html.parser')
+
+ul = soup.find(
+    'ul', attrs={'class': 'b-list__items_nofooter srp-results srp-grid'})
+
+
+titles = []
+prices = []
+shippings = []
+urls = []
+
+for items in ul.findAll('li', attrs={'class': 's-item'}):
+    items.find('h3', attrs={'class': 's-item__title'})
